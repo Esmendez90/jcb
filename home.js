0027b03cd1356ff77289702ec4e28617940ca1a9
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2021-08-17 12:07:54 -0400

    Fixing app

--- home.js
+++ home.js
@@ -14,6 +14,9 @@ let sale = false;
 let stateInit =
   "AK AL AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY";
 
+console.log(key);
+console.log(host);
+
 $("#search_btn").on("click", function (event) {
   event.preventDefault();
   state = $("#state_search").val().trim().toUpperCase();
