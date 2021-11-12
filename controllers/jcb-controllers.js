8fe98027410d4433faeba061f450518fdbd85362
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2021-11-12 15:35:15 -0500

    added routes for rentals and forsale pages

--- controllers/jcb-controllers.js
+++ controllers/jcb-controllers.js
@@ -4,6 +4,13 @@ const router = express.Router();
 
 // Routes
 // =========================================================
+router.get("/rentals", function (req, res) {
+  res.render("rentals");
+});
+
+router.get("/forsale", function (req, res) {
+  res.render("forsale");
+});
 
 router.get("*", function (req, res) {
   res.render("index");
