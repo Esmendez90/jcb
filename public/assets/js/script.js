496092a9cb8b857c31147397fad494db25e8b6a0
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2021-11-23 21:05:32 -0500

    style added icons

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,111 +1,26 @@
+console.log("is this even working");
 let rentalListing = document.getElementById("rentalListing");
-let propStatus = document.getElementById("propStatus");
-let citySearch = document.getElementById("city_search");
-let stateSearch = document.getElementById("state_search");
-let zipCodeContainer = document.getElementById("zipCodeContainer");
-let codeInput = document.getElementById("codeInput");
-let city;
-let state;
-let url;
-let rent = true;
-let sale = false;
-let stateInit =
-  "AK AL AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY";
+let city = "west new york";
+city = encodeURI(city);
+console.log(city);
 
-$("#search_btn").on("click", function (event) {
-  event.preventDefault();
-  state = $("#state_search").val().trim().toUpperCase();
-  city = encodeURI($("#city_search").val().trim());
-  if ($.inArray(state, stateInit.split(" ")) >= 0) {
-    choosePropStatus();
-  } else if (citySearch.value === "" || stateSearch.value === "") {
-    alert("Please, enter a city and state to complete your search.");
-  } else {
-    alert("Please, enter valid U.S state initials.");
-  }
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: `https://realty-in-us.p.rapidapi.com/properties/list-for-rent?state_code=NJ&city=${city}&limit=200&offset=0&sort=relevance`,
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "realty-in-us.p.rapidapi.com",
+    "x-rapidapi-key": "fbf1849f58mshc4a745d4d24f860p128e2fjsn3c711de2d5ef",
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response.listings);
+  showListings(response.listings);
 });
 
-$("#searchByCode").on("click", function (event) {
-  event.preventDefault();
-  enterZipCode();
-});
 
-$("#searchForSaleByCode").on("click", function (event) {
-  event.preventDefault();
-  rent = false;
-  sale = true;
-  enterZipCode();
-});
-
-// Choose between rentals or for sale
-function choosePropStatus() {
-  propStatus.style.display = "block";
-  // Rental button
-  $("#rentalBtn").on("click", function (event) {
-    event.preventDefault();
-    url = `https://realty-in-us.p.rapidapi.com/properties/list-for-rent?city=${city}&state_code=${state}&limit=200&offset=0&sort=relevance`;
-    
-    searchProperties(url);
-    propStatus.style.display = "none";
-  });
-
-  // On sale button
-  $("#onSaleBtn").on("click", function (event) {
-    event.preventDefault();
-    url = `https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=${state}&city=${city}&offset=0&limit=200&sort=relevance`;
-   // console.log(url);
-    searchProperties(url);
-    propStatus.style.display = "none";
-  });
-}
-
-// Enter zip code input
-function enterZipCode() {
-  zipCodeContainer.style.display = "block";
-
-  $("#zipCodeBtn").on("click", function (event) {
-    event.preventDefault();
-    if (codeInput.value === "") {
-      alert("Please, enter a valid zip code to complete your search.");
-    } else if (rent && !sale) {
-      codeInput = $("#codeInput").val().trim();
-      url = `https://realtor.p.rapidapi.com/properties/list-for-rent?state_code=0&city=0&limit=200&offset=0&postal_code=${codeInput}&sort=relevance`;
-      searchProperties(url);
-    } else if (!rent && sale) {
-      codeInput = $("#codeInput").val().trim();
-      url = `https://realtor.p.rapidapi.com/properties/list-for-sale?state_code=0&city=0&limit=200&offset=0&postal_code=${codeInput}&sort=relevance`;
-      searchProperties(url);
-    }
-  });
-}
-
-// Search properties
-function searchProperties(url) {
-  //console.log(url);
-  const settings = {
-    async: true,
-    crossDomain: true,
-    url: url,
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "fbf1849f58mshc4a745d4d24f860p128e2fjsn3c711de2d5ef",
-      "x-rapidapi-host": "realty-in-us.p.rapidapi.com",
-    },
-  };
-  $.ajax(settings).done(function (response) {
-    let listings = response.listings;
-    //console.log(listings);
-    // let listingserties = [];
-    // for (var i = 0; i < 20; i++) {
-    //   let rand = listings[Math.floor(Math.random() * 200)];
-    //   listingserties.push(rand);
-    // }
-    // console.log(listingserties);
-    showListings(listings);
-  });
-}
-
-// Property Listings
 function showListings(listings) {
   rentalListing.style.display = "block";
   for (let i = 0; i < listings.length; i++) {
