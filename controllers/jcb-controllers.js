b84413ae701e6df2d8b71c556a3833b83f4472ce
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2021-11-27 15:33:28 -0500

    deploying

--- controllers/jcb-controllers.js
+++ controllers/jcb-controllers.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const router = express.Router();
-var axios = require("axios").default;
 
 // Routes
 // =========================================================
+router.get("/", function (req, res) {
+  res.render("index");
+});
+
 router.get("/rentals", function (req, res) {
   res.render("rentals");
 });
 
 router.get("/forsale", function (req, res) {
   res.render("forsale");
-});
-
-router.get("*", function (req, res) {
-  res.render("index");
 });
 
 module.exports = router;
