c1198f8dbf45de5e865848b9018180204c4385d7
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2021-12-03 15:24:51 -0500

    hide keys

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,4 +1,4 @@
-const config = { // this should be const instead if you're using ES6 standards
+const config = {
     apiKey : 'fbf1849f58mshc4a745d4d24f860p128e2fjsn3c711de2d5ef',
     host: 'realty-in-us.p.rapidapi.com'
   }
