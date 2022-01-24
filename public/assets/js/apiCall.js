39c4ded7ff3b01edbad8d8adfaa97e70ccd6e2cb
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-01-24 13:39:27 -0500

    edited header, moved stickyRow to be part of the header

--- public/assets/js/apiCall.js
+++ public/assets/js/apiCall.js
@@ -17,8 +17,6 @@ let zipCode;
    apiCall("union city", "07087");
 }
 
-
-
 $(".prop-form").on("submit", function (event) {
   event.preventDefault();
 
@@ -34,7 +32,7 @@ function apiCall(cityName, zipCode) {
   const settings = {
     async: true,
     crossDomain: true,
-    url: `https://realty-in-us.p.rapidapi.com/properties/list-for-${propTypeUrl}?state_code=NJ&city=${cityName}&limit=200&offset=0&postal_code=${zipCode}`,
+    url: `https://realty-in-us.p.rapidapi.com/properties/v2/list-for-${propTypeUrl}?state_code=NJ&city=${cityName}&limit=200&offset=0&postal_code=${zipCode}`,
     method: "GET",
     headers: {
       "x-rapidapi-host": config.host,
@@ -43,47 +41,48 @@ function apiCall(cityName, zipCode) {
   };
 
   $.ajax(settings).done(function (response) {
-    console.log(propTypeUrl);
-    cityName = decodeURI(cityName);
-    $(
-      "#resultsHTML"
-    )[0].innerHTML = `Results for ${propTypeUrl} in <strong>${cityName}</strong>: ${response.returned_rows} found.`;
-    showListings(response.listings);
-  });
+    console.log(response.properties);
+    // console.log(propTypeUrl);
+    // cityName = decodeURI(cityName);
+  //   $(
+  //     "#resultsHTML"
+  //   )[0].innerHTML = `Results for ${propTypeUrl} in <strong>${cityName}</strong>: ${response.returned_rows} found.`;
+    showListings(response.properties);
+   });
 }
 
 function showListings(listings) {
   $(".cardListing").remove();
 
   for (let i = 0; i < listings.length; i++) {
-    let propPhoto = listings[i].photo;
+    let propPhoto = listings[i].photos;
     let propStatus = listings[i].prop_status;
     let propPrice = listings[i].price;
-    let propAddress = listings[i].address;
-    let propBaths = listings[i].baths;
-    let propBeds = listings[i].beds;
+    let propAddress = listings[i].address.line;
+    // let propBaths = listings[i].baths;
+    // let propBeds = listings[i].beds;
 
-    if (propPhoto) {
-      propPhoto = listings[i].photo;
-    } else if ((propPhoto = "undefined")) {
-      propPhoto =
-        "https://images.freeimages.com/images/large-previews/338/house-2-1225477.jpg";
-    }
+    // if (propPhoto) {
+    //   propPhoto = listings[i].photo;
+    // } else if ((propPhoto = "undefined")) {
+    //   propPhoto =
+    //     "https://images.freeimages.com/images/large-previews/338/house-2-1225477.jpg";
+    // }
 
-    $("#propListing").append(
-      `
-        <div class="card cardListing">
-        <i class="far fa-star"></i>
-          <img src="${propPhoto}" class="card-img-top propertyImg" alt="Property house" />
-            <div class="card-body">
-               <p class="card-text">PRICE: <span>${propPrice}</span></p>
-               <p class="card-text">ADDRESS: <span>${propAddress}</span></p>
-               <p class="card-text">BATHROOMS: <span>${propBaths}</span></p>
-               <p class="card-text">BEDS: <span>${propBeds}</span></p>
-               <p class="card-text">STATUS: <span>${propStatus}</span></p>
-            </div>
-        </div>
-        `
-    );
+    // $("#propListing").append(
+    //   `
+    //     <div class="card cardListing">
+    //     <i class="far fa-star"></i>
+    //       <img src="${propPhoto}" class="card-img-top propertyImg" alt="Property house" />
+    //         <div class="card-body">
+    //            <p class="card-text">PRICE: <span>${propPrice}</span></p>
+    //            <p class="card-text">ADDRESS: <span>${propAddress}</span></p>
+    //            <p class="card-text">BATHROOMS: <span>${propBaths}</span></p>
+    //            <p class="card-text">BEDS: <span>${propBeds}</span></p>
+    //            <p class="card-text">STATUS: <span>${propStatus}</span></p>
+    //         </div>
+    //     </div>
+    //     `
+    // );
   }
 }
